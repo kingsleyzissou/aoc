9ffeced914e26d326da9a6d04b4d0dca5a19abd2
Gianluca Zuccarelli <gzuccare@redhat.com>
2023-12-05 02:09:14 +0000

    day4/s2: second star

--- src/bin/day4/main.rs
+++ src/bin/day4/main.rs
@@ -48,6 +48,36 @@ fn star1(input: Vec<String>) -> i32 {
         .sum();
 }
 
+fn star2(input: Vec<String>) -> i32 {
+    let mut cards = input
+        .iter()
+        .map(|s| s.split(": ").nth(1).unwrap())
+        .enumerate()
+        .map(|(i, s)| parse_line(i, &String::from(s)))
+        .rev()
+        .collect::<Vec<Card>>();
+
+    // this makes things slightly more efficient
+    let mut lookup = HashMap::new();
+    for card in cards.clone() {
+        lookup.insert(card.index, card);
+    }
+
+    let mut copies = cards.len() as i32;
+
+    // still not super efficient :[
+    while cards.len() > 0 {
+        let card = cards.pop().unwrap();
+        copies += card.winners;
+        for i in card.index + 1..card.index + card.winners as usize + 1 {
+            let c = lookup.get(&i).unwrap();
+            cards.push(c.clone());
+        }
+    }
+
+    return copies;
+}
+
 fn main() {
     let input = File::open("input/d4.txt").unwrap();
     let input = BufReader::new(input)
@@ -55,6 +85,7 @@ fn main() {
         .map(|l| l.unwrap())
         .collect::<Vec<String>>();
     println!("Star 1: {}", star1(input.clone()));
+    println!("Star 2: {}", star2(input.clone()));
 }
 
 #[cfg(test)]
@@ -76,5 +107,18 @@ mod tests {
             "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
         ]);
         assert_eq!(star1(input), 13);
+    }
+
+    #[test]
+    fn test_star2() {
+        let input = to_string_vec(vec![
+            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+            "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+            "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+            "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+            "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+            "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+        ]);
+        assert_eq!(star2(input), 30);
     }
 }
