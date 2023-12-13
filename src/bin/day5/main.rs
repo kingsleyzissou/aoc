1de8be9fb998a0a2fac33f972d8ec880056d6fa1
Gianluca Zuccarelli <gzuccare@redhat.com>
2023-12-13 00:02:46 +0000

    day5/s2: second star ⭐⭐

--- src/bin/day5/main.rs
+++ src/bin/day5/main.rs
@@ -13,6 +13,37 @@ impl Range {
     fn new(start: u64, end: u64) -> Range {
         return Range { start, end };
     }
+
+    fn intersection(&self, other: Range) -> Vec<Range> {
+        let start = std::cmp::max(self.start, other.start);
+        let end = std::cmp::min(self.end, other.end);
+        let mut tmp: Vec<Range> = Vec::new();
+
+        if start < end {
+            let intersection = Range::new(start, end);
+            tmp.push(intersection);
+
+            // perfect intersection
+            if self.start == start && self.end == end {
+                return tmp;
+            }
+
+            // left overlap
+            if self.start < start {
+                tmp.push(Range::new(self.start, start - 1));
+            }
+
+            // right overlap
+            if end < self.end {
+                tmp.push(Range::new(end + 1, self.end))
+            }
+
+            return tmp;
+        }
+
+        // return an empty vector
+        return tmp;
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -25,16 +56,54 @@ impl Mapping {
     fn new(src: Range, dest: Range) -> Mapping {
         return Mapping { src, dest };
     }
+
+    fn transform(&self, range: Range) -> Range {
+        // we're dealing with unsigned ints!
+        if self.src.start >= self.dest.start {
+            let delta = self.src.start - self.dest.start;
+            return Range::new(range.start - delta, range.end - delta);
+        }
+        let delta = self.dest.start - self.src.start;
+        return Range::new(range.start + delta, range.end + delta);
+    }
 }
 
 fn contains_or(maps: &Vec<Mapping>, needle: Range) -> Range {
     for map in maps.clone() {
         if map.src.start < needle.start && needle.start < map.src.end {
             let diff = map.dest.start + needle.start - map.src.start;
-            return Range::new(diff, diff + 1);
+            return Range::new(diff, diff);
         }
     }
     return needle;
+}
+
+fn intersects_or(mappings: &Vec<Mapping>, needle: Range) -> Vec<Range> {
+    let intersections: Vec<Range> = mappings
+        .iter()
+        .map(|mapping| needle.intersection(mapping.src))
+        .filter(|mapping| mapping.len() > 0)
+        .map(|ranges| {
+            let mut transformations = Vec::new();
+            for r in ranges {
+                for m in mappings {
+                    // off by one error that I can't figure out.
+                    // so I'll just include the end \o/
+                    if r.start >= m.src.start && r.end <= m.src.end {
+                        transformations.push(m.transform(r));
+                    }
+                }
+            }
+            return transformations;
+        })
+        .flat_map(|range| range.into_iter())
+        .collect();
+
+    if !intersections.is_empty() {
+        return intersections;
+    }
+
+    return vec![needle];
 }
 
 fn parse_mapping(line: &str) -> Mapping {
@@ -85,6 +154,31 @@ fn star1(input: Vec<String>) -> u64 {
         });
 }
 
+fn star2(input: Vec<String>) -> u64 {
+    let seeds: Vec<Range> = parse_seeds(input.iter().nth(0).expect("input to have multiple lines"))
+        .chunks_exact(2)
+        // this is partially responsible for the off by 1 error
+        .map(|chunk| Range::new(chunk[0], chunk[0] + chunk[1] - 1))
+        .collect();
+
+    return parse_mappings(input.clone())
+        .iter()
+        .fold(seeds.clone(), |acc, mapping| {
+            let mut intersections = Vec::new();
+            for seed in acc.iter() {
+                let i = intersects_or(mapping, *seed);
+                intersections.extend(i);
+            }
+            return intersections;
+        })
+        .into_iter()
+        .sorted_by(|a, b| Ord::cmp(&a.start, &b.start))
+        .collect::<Vec<Range>>()
+        .first()
+        .unwrap()
+        .start;
+}
+
 fn load_input() -> io::Result<Vec<String>> {
     let file = File::open("input/d5.txt")?;
     let mut input = String::new();
@@ -101,6 +195,7 @@ fn load_input() -> io::Result<Vec<String>> {
 fn main() {
     let input = load_input().expect("input should load fine");
     println!("star 1: {}", star1(input.clone()));
+    println!("star 2: {}", star2(input.clone()));
 }
 
 #[cfg(test)]
@@ -156,5 +251,16 @@ humidity-to-location map:
                 .collect::<Vec<&str>>(),
         );
         assert_eq!(star1(input), 35);
+    }
+
+    #[test]
+    fn test_star2() {
+        let input = to_string_vec(
+            TEST_INPUT
+                .split("\n\n")
+                .map(|s| s.trim())
+                .collect::<Vec<&str>>(),
+        );
+        assert_eq!(star2(input), 46);
     }
 }
